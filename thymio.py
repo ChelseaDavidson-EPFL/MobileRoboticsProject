0bf57f84157fcdb679344b995cd0578385fd2619
abauer <arthur.bauer@epfl.ch>
2025-11-19 02:51:39 +0100

    implementing ir reading and motor speeds in thymio class

--- thymio.py
+++ thymio.py
@@ -1,5 +1,10 @@
+import tdmclient.notebook
+from tdmclient import ClientAsync, aw
+import time
+
+
 class Thymio :
-    ir_sensors = [0, 0, 0, 0, 0, 0]
+    ir_sensors = [0, 0, 0, 0, 0]
     motor_speeds = [0, 0]
     pos = [0, 0]
     orient = 0
@@ -8,25 +13,41 @@ class Thymio :
 
     # Constructor
     def __init__(self, pos_init, orient):
+        self.client = ClientAsync()
+        self.node = None
         self.pos = pos_init
         self.orient = orient
         self.nav_mode = "GLOBAL"
-        self.ir_sensors = self.get_ir()
-        self.set_motor_speeds(0, 0)
+        self.ir_sensors = [0, 0, 0, 0, 0]
+        self.motor_speeds = [0, 0]
 
     # Methods
-    def set_motor_speeds(self, left_speed, right_speed):
-        self.motor_speeds[0] = left_speed
-        self.motor_speeds[1] = right_speed
-        # TODO : Set the motor speeds on the Thymio robot
+
+    async def _connect_to_thymio_(self):
+        self.node = await self.client.wait_for_node()
+        print("Thymio connected")
+        await self.node.lock()
 
 
-    def get_ir(self):
-        # TODO : Get the IR sensor values from the Thymio robot
-        return self.ir_sensors
+    def set_motor_speeds(self, speeds):
+        
+        self.node.flush()
+        self.motor_speeds = speeds
+        self.node.send_set_variables({"motor.left.target": [speeds[0]], "motor.right.target": [speeds[1]]})
+
+
+    async def update_ir(self):
+        self.node.flush()
+        await self.node.wait_for_variables({"prox.horizontal"})
+        if "prox.horizontal" in self.node:
+            self.ir_sensors = self.node["prox.horizontal"]
+            self.ir_sensors = self.ir_sensors[0:5]
 
     def set_pos(self, new_pos, orient):
         self.pos = new_pos
         self.orient = orient
         # TODO : Update the position on the Thymio robot if necessary
+    
+    def stop(self):
+        self.set_motor_speeds([0, 0])
     
